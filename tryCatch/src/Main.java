0d2c03eb85243dabf333a8042d12366d7f8794f7
BatistaQ <kauanbs29@hotmail.com>
2024-09-10 20:05:43 -0300

    tratamento de excecoes

---
+++ tryCatch/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        method1();
+        System.out.println("End of program");
+    }
+
+    public static void method1(){
+        System.out.println("***Method1 Start");
+        method2();
+        System.out.println("***Method1 End***");
+    }
+
+    public static void method2 (){
+        System.out.println("***Method2 Start***");
+        Scanner sc = new Scanner(System.in);
+        try {
+            String[] vect = sc.nextLine().split(" ");
+            int position = sc.nextInt();
+            System.out.println(vect[position]);
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Invalid Position");
+            e.printStackTrace();
+            sc.next();
+        }
+        catch (InputMismatchException e){
+            System.out.println("Input Error!");
+        }
+        System.out.println("***Method2 End***");
+        sc.close();
+    }
+}
