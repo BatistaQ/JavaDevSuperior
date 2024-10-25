ac607d5ec81bf44d580080f8b30676357c965b9d
BatistaQ <kauanbs29@hotmail.com>
2024-10-25 12:09:13 -0300

    exercicio fixacao sobre Map

---
+++ Map/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String,String> cookies = new TreeMap<>();
+
+        cookies.put("username","Maria");
+        cookies.put("email","maria@gmail.com");
+        cookies.put("phone","965458465");
+
+        cookies.remove("email");
+        cookies.put("phone", "965458113");
+
+        System.out.println("Cotains 'phone' key: " + cookies.containsKey("phone") );
+        System.out.println("Phone number: " + cookies.get("phone"));
+        System.out.println("Email: " + cookies.get("email"));
+        System.out.println("Size: " + cookies.size());
+
+
+        System.out.println();
+        System.out.println("All cookies: ");
+        for (String key : cookies.keySet()){
+            System.out.println(key + ": " + cookies.get(key));
+        }
+    }
+}
